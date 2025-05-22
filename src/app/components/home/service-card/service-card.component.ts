8e01994cb0abef1642b4c3a1c6d0e01b639911ce
SirAxLord <A325641@alumnos.uaslp.mx>
2025-05-22 08:54:52 -0600

    Servicios ya cargan desde el backend

--- src/app/components/home/service-card/service-card.component.ts
+++ src/app/components/home/service-card/service-card.component.ts
@@ -1,20 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';     // Para el icono
-import { MatButtonModule } from '@angular/material/button';   // Para el botón "link"
-import { Service } from '../services-section/services-section.component'; // Importa la interfaz
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';  
+import { Service } from '../../../services/service.service'; 
 
 @Component({
   selector: 'app-service-card',
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule,    // <-- Importa módulos necesarios
+    MatIconModule,
     MatButtonModule
   ],
   templateUrl: './service-card.component.html',
   styleUrls: ['./service-card.component.css']
 })
 export class ServiceCardComponent {
-  @Input() service!: Service; // Recibe el objeto 'service' del padre
+  @Input() service!: Service;
 }
