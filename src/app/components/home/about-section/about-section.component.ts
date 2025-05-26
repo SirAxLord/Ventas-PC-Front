36d4e135a294fcfd45d9cbc6510a1155f0184e33
SirAxLord <A325641@alumnos.uaslp.mx>
2025-05-26 12:27:50 -0600

    imagen local

--- src/app/components/home/about-section/about-section.component.ts
+++ src/app/components/home/about-section/about-section.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button'; // Para el botón
+import { MatButtonModule } from '@angular/material/button'; 
 
 @Component({
   selector: 'app-about-section',
   standalone: true,
   imports: [
     CommonModule,
-    MatButtonModule // Importa el módulo del botón
+    MatButtonModule 
   ],
   templateUrl: './about-section.component.html',
   styleUrls: ['./about-section.component.css']
 })
 export class AboutSectionComponent {
-  // No necesita lógica especial ni datos por ahora
+
 }
